50396f7604041ebce04b8587a354d4aaa5d9b07f
numkmr <kamonruksariyarsheeva@gmail.com>
2019-04-03 16:33:48 +0700

    num na ngoo

--- src/pages/resulthistperc/resulthistperc.ts
+++ src/pages/resulthistperc/resulthistperc.ts
@@ -27,11 +27,11 @@ export class ResulthistpercPage {
   max: number = 100;
   stroke: number = 20;
   radius: number = 75;
-  semicircle: boolean = true;
+  semicircle: boolean = false;
   rounded: boolean = true;
   responsive: boolean = false;
   clockwise: boolean = true;
-  color: string = '#45ccce';
+  color: string = '#f78710';
   background: string = '#eaeaea';
   duration: number = 800;
   animation: string = 'linearEase';
